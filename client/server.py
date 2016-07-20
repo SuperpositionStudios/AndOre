4bef09e14a6ce4874fc746d959a315f4639c0e3f
JCharante <ralphcharante@gmail.com>
2016-07-20 18:06:41 -0400

    Finished creating the client server; added details to readme

--- client/server.py
+++ client/server.py
@@ -1,1 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
 app.run(debug=True, host='0.0.0.0', port=7002,threaded=True)
