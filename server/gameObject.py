4dfc43f1a812441840a03eef10161daa40345948
JCharante <ralphcharante@gmail.com>
2016-07-20 23:48:08 -0400

    Added passable properties to game objects

--- server/gameObject.py
+++ server/gameObject.py
@@ -4,6 +4,7 @@ import uuid
 class GameObject:
     def __init__(self):
         self.obj_id = str(uuid.uuid4())
+        self.passable = True
 
 
 class OreDeposit(GameObject):
@@ -12,6 +13,7 @@ class OreDeposit(GameObject):
         super().__init__()
         self.icon = '$'
         self.cell = _cell
+        self.passable = False
 
 
 class EmptySpace(GameObject):
