00e14fa914e4e3ebb116e9413612ae3533f398a9
JCharante <ralphcharante@gmail.com>
2016-07-25 14:42:18 -0400

    Updated pip requirements

--- server/server.py
+++ server/server.py
@@ -16,10 +16,8 @@ web_server_domain = "*"
 
 
 world = World()
-#world.world[4][4].add_ore_deposit()
 world.spawn_ore_deposits(20)
 world.spawn_hospitals(20)
-#world.world[8][8].add_hospital()
 
 
 def restart_program():
