17ecab20d0588918b081d5bd10ff41f7d657ac08
JCharante <ralphcharante@gmail.com>
2016-07-27 16:03:56 -0400

    Bug fixes to database_functions

--- ai_storage/database_functions.py
+++ ai_storage/database_functions.py
@@ -12,18 +12,18 @@ session = DBSession()
 
 
 def save_model(mid, data):
-    if session.query(Model).filter(Model.mid == mid) is None:
+    if session.query(Model).filter(Model.mid == mid).first() is None:
         session.add(Model(mid=mid, data=data))
         session.commit()
         return "Saved new model"
     else:
-        session.query(Model).filter(Model.mid == mid).data = data
+        session.query(Model).filter(Model.mid == mid).first().data = data
         session.commit()
         return "Updated existing model"
 
 
 def retrieve_model(mid):
-    if session.query(Model).filter(Model.mid == mid) is None:
-        return "Model with entered id {} not found".format(mid)
+    if session.query(Model).filter(Model.mid == mid).first() is None:
+        return None
     else:
-        return session.query(Model).filter(Model.mid == mid).data
+        return session.query(Model).filter(Model.mid == mid).first().data
