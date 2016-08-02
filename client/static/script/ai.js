353c12d92aa182a03ce6665c2ea543977da15c6b
halstedlarsson <halsted.larsson@gmail.com>
2016-08-02 10:37:20 -0400

    in progress - ai refactor
    beginning to factor out ai code

---
+++ client/static/script/ai.js
@@ -0,0 +1,155 @@
+//This lets us make different kinds of ais
+
+
+
+
+BaseAi = function(app) {
+  this.app = app;
+}
+
+BaseAi.prototype = {
+  app: null,
+  actions: [], 
+  dataSize: 1024,
+  lastHealth: 0,
+  hasActed: false,
+  lastAction: null,
+  lastVitals: null,
+  GetModel: function() {
+    var self = this;
+    data = {
+        'mid': prompt("What is the AI's Name?")
+    };
+    ai_name = data['mid'];
+    $.ajax({
+        type: "POST",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        url: ai_storage_endpoint + "/retrieve",
+        data: JSON.stringify(data),
+        success: function(_data) {
+            console.log(_data);
+            self.oldBrain = JSON.parse(_data['model']);
+            console.log("Retrieved and saved AI Model into memory");
+            self.StartAi();
+        },
+    });
+  },
+  NewEnv: function() {
+    var self = this;
+    return {
+      getNumStates: function { return self.dataSize; },
+      getMaxNumActions: function() { return self.actions.length; },
+      allowedActions: function() { return self.actions; },
+    }
+  },
+  StartAi: function() {
+    this.actions = this.actions.length > 0? this.actions : app.keys.keys();
+    this.env = this.NewEnv();
+    //var oldBrain = localStorage.getItem("aiModel");
+    var spec = { alpha: 0.01 };
+    this.agent = new RL.DQNAgent(self.env, spec);  
+    if(this.oldBrain != null){
+      this.agent.fromJSON(this.oldBrain);
+      console.log("Parsed stored model into Agent.");
+    }
+    this.AiTick();
+  },
+  Tick: function(){
+    var self = this;
+    var repeat;
+    repeat = function() {
+      var command = self.keys[self.lastAction];
+      AjaxCall("/action", {id: userId, action: command, sendState:true}, function(data){ 
+        var worldAge = data.vitals.world_age;
+        if (worldAge > self.lastAge) {
+          view.Draw(data.world);
+          self.UpdateAi(data, repeat);
+        } else {
+          view.Draw(data.world);
+          setTimeout(repeat, self.delay);
+        }
+      }, repeat);
+    }
+    repeat();
+  },
+  Update: function(data) {
+    var env = this.env;
+
+    if(this.lastVitals == null){
+    this.lastVitals = data.vitals;
+    }
+
+    this.lastAge = data.vitals.world_age;
+      
+    var state = this.FlattenWorld(data.world);
+    var deltaHealth = data.vitals.health - this.lastHealth;
+    var reward = (data.vitals.delta_ore * 2 + deltaHealth * 1) / 3;
+
+    if (data.vitals.row == this.lastVitals.row && data.vitals.col == this.lastVitals.col){
+      reward -= 0.5;
+    } else {
+      reward += 0.2;
+    }
+
+
+    this.lastVitals = data.vitals;
+
+    this.lastHealth = data.vitals.health;
+
+    if(this.lastHealth < 10) {
+      reward = -5;
+    }
+
+    if(this.lastAction != null){  
+      this.agent.learn(reward);      
+    }
+
+    var action = this.agent.act(this.actions);
+    if(this.lastAction != action){
+      this.newAction = true;
+    } else {
+      this.newAction = false;
+    }
+    this.lastAction = action;    
+  },
+  UploadModel: function(ai_model) {
+    console.log("Uploading Model...");
+    data = {
+        'mid': ai_name,
+        'model': ai_model
+    }
+    $.ajax({
+      url: ai_storage_endpoint + "/upload",
+      type: "POST",
+      data: JSON.stringify(data),
+      dataType: "json",
+      contentType: "application/json; charset=utf-8",
+      success: function(_data) {
+        console.log(_data);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log("Error uploading the model");
+      }
+    });
+  },
+  FlattenWorld: function(world){
+    var state = [];
+    for (var i in world){
+      var line = world[i];
+      for (var key in line){
+        state.push(line[key].charCodeAt(0));
+      }    
+    }
+    while( state.length < this.dataSize){
+      state.push(" ");
+    }
+    while( state.length > this.dataSize){
+      state.pop();
+    }
+    return state;
+  }
+}
+
+
+
--- client/static/script/view.js
+++ client/static/script/view.js
@@ -4,30 +4,6 @@
 //called by app.js after id is populated, etc
 var contentId = "content";
 var pollDelay = 350;
-var validKeys = {
-    "w": true,  // Direction Key
-    "a": true,  // Direction Key
-    "s": true,  // Direction Key
-    "d": true,  // Direction Key
-    "m": true,  // Primary Modifier Key
-    "k": true,  // Primary Modifier Key
-    "l": true,  // Primary Modifier Key
-    "i": true,  // Primary Modifier Key
-    "-": true,  // Primary Modifier Key
-    "+": true,  // Primary Modifier Key
-    "b": true,  // Primary Modifier Key
-    "u": true,  // Primary Modifier Key
-    "0": true,  // Secondary Modifier Key
-    "1": true,  // Secondary Modifier Key
-    "2": true,  // Secondary Modifier Key
-    "3": true,  // Secondary Modifier Key
-    "4": true,  // Secondary Modifier Key
-    "5": true,  // Secondary Modifier Key
-    "6": true,  // Secondary Modifier Key
-    "7": true,  // Secondary Modifier Key
-    "8": true,  // Secondary Modifier Key
-    "9": true   // Secondary Modifier Key
-};
 var view = {
   contentDiv: null,
   SetupView : function(callback) {
@@ -52,9 +28,9 @@ var view = {
   },
   SetupInput: function() {
     $("body").keypress(function(e){
-      command = String.fromCharCode(e.which).toLowerCase()
+      command = String.fromCharCode(e.which).toLowerCase();
       //console.log(command);
-      if(validKeys[command]) {
+      if(app.keys[command]) {
         app.SendCommand(command);
       }
     });
