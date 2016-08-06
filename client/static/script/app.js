c5fe70392e92d3cf85e4be66d26f2fe849430e54
JCharante <ralphcharante@gmail.com>
2016-08-05 20:39:49 -0400

    Moved modal functions from outside App to inside.

--- client/static/script/app.js
+++ client/static/script/app.js
@@ -11,7 +11,7 @@ var dev_game_server_endpoint = ":7001";
 var dev_ai_storage_endpoint = ":7003";
 var dev_auth_server_endpoint = ":7004";
 
-var use_dev_server = true;  // Used for development
+var use_dev_server = false;  // Used for development
 var use_ai_storage_server = true;
 var internetOff = false;  // Used for testing view.js with testData.js
 
@@ -38,8 +38,6 @@ function ArrayToKeys(inArray) {
   }
   return out;
 }
-var auth_id = null;
-var game_id = null;
 
 function App() {
 
@@ -48,6 +46,8 @@ function App() {
 App.prototype = {
   delay: 10,
   hasActed: false,
+  authId: null,
+  gameId: null,
   userId: null,
   startAiKey: '~',
   AiStarted: false,
@@ -87,18 +87,134 @@ App.prototype = {
   Init: function() {
     var self = this;
     this.actionsLut = ArrayToKeys(this.actions);
+    /*
   	this.GetUserId(function(){
       self.view = new View();
       self.view.SetupView(this, App.GetDisplay);
-  	});
+  	});*/
+    self.GetAuthId(function() {
+      self.GetGameId(function() {
+        self.ListenToStartAi(function() {
+          self.view = new View();
+          self.view.SetupView(this, App.GetDisplay);
+        });
+      });
+    })
   },
-
-
-  GetUserId:function(callback) {
+  GetAuthId: function (callback) {
     var self = this;
-    //AjaxCall('game server', "/join", {sendState: false}, function(data) {
-    //});
-    userId = game_id;
+    $('#modal1').openModal({
+      dismissible: false
+    });
+    $('#login').click(function() {
+      var username = $('#login_username').val();
+      var password = $('#login_password').val();
+      var data = {
+        username: username,
+        password: password
+      };
+      $.ajax({
+        method: 'POST',
+        url: auth_server_endpoint + '/account/login',
+        data: JSON.stringify(data),
+        dataType: "json",
+        contentType: "application/json",
+        success: function(data) {
+          if (data['status'] == 'Success') {
+            self.authId = data['uid'];
+            $('#modal1').closeModal();
+            CallCallback(callback);
+            //console.log(auth_id);
+          } else {
+            console.log(data);
+          }
+        }
+
+      });
+    });
+    $('#signup').click(function() {
+      var username = $('#signup_username').val();
+      var password = $('#signup_password').val();
+      var data = {
+        username: username,
+        password: password
+      };
+      $.ajax({
+        method: 'POST',
+        url: auth_server_endpoint + '/account/create',
+        data: JSON.stringify(data),
+        dataType: "json",
+        contentType: "application/json",
+        success: function(data) {
+          if (data['status'] == 'Success') {
+            self.authId = data['uid'];
+            $('#modal1').closeModal();
+            CallCallback(callback);
+            //console.log(auth_id);
+          } else {
+            console.log(data);
+          }
+        }
+
+      });
+    });
+  },
+  GetGameId: function (callback) {
+    var self = this;
+    $('#modal2').openModal({
+      dismissible: false
+    });
+
+    $('#rejoin_game').click(function() {
+      var data = {
+        uid: self.authId
+      };
+      $.ajax({
+        method: 'POST',
+        url: auth_server_endpoint + '/game/rejoin',
+        data: JSON.stringify(data),
+        dataType: "json",
+        contentType: "application/json",
+        success: function(data) {
+          if (data['status'] == 'Success') {
+            self.gameId = data['game-id'];
+            self.userId = data['game-id'];
+            $('#modal2').closeModal();
+            CallCallback(callback);
+          } else {
+            console.log(data);
+          }
+        }
+
+      });
+    });
+    $('#start_anew').click(function() {
+      var data = {
+        uid: self.authId
+      };
+      $.ajax({
+        method: 'POST',
+        url: auth_server_endpoint + '/game/join',
+        data: JSON.stringify(data),
+        dataType: "json",
+        contentType: "application/json",
+        success: function(data) {
+          if (data['status'] == 'Success') {
+            self.gameId = data['game-id'];
+            self.userId = data['game-id'];
+            $('#modal2').closeModal();
+            CallCallback(callback);
+          } else {
+            console.log(data);
+          }
+        }
+
+      });
+    });
+  },
+  ListenToStartAi:function(callback) {
+    var self = this;
+
     $("body").keypress(function(e) {
       if (String.fromCharCode(e.which) == self.startAiKey && self.AiStarted == false) {
         self.ai = new SimpleAi(self);
@@ -113,18 +229,20 @@ App.prototype = {
     CallCallback(callback);
   },
   GetDisplay: function(callback) {
+    var self = this;
     var view = this.view;
-  	AjaxCall('game server', "/sendState", {id: userId}, function(data){
+  	AjaxCall('game server', "/sendState", {id: self.userId}, function(data){
       view.Draw(data);
       CallCallback(callback);
   	});
   },
   SendCommand: function(command){
+    var self = this;
     var view = this.view;
-    AjaxCall('game server', "/action", {id: userId, action: command, sendState:true}, function(data){
+    AjaxCall('game server', "/action", {id: self.userId, action: command, sendState:true}, function(data){
       view.Draw(data);
     });
-  },
+  }
 };
 
 function CallCallback (callback){
@@ -164,111 +282,7 @@ function AjaxCall(server, endpoint, data, callback, failCallback){
 
 
 $(function(){
-  $('#modal1').openModal({
-    dismissible: false
-  });
-  $('#rejoin_game').click(function() {
-    var data = {
-      uid: auth_id
-    };
-    $.ajax({
-      method: 'POST',
-      url: auth_server_endpoint + '/game/rejoin',
-      data: JSON.stringify(data),
-      dataType: "json",
-      contentType: "application/json",
-      success: function(data) {
-        if (data['status'] == 'Success') {
-          game_id = data['game-id'];
-          $('#modal2').closeModal();
-          app = new App();
-          var ai = new BaseAi(app);
-          app.Init();
-        } else {
-          console.log(data);
-        }
-      }
-
-    });
-  });
-  $('#start_anew').click(function() {
-    var data = {
-      uid: auth_id
-    };
-    $.ajax({
-      method: 'POST',
-      url: auth_server_endpoint + '/game/join',
-      data: JSON.stringify(data),
-      dataType: "json",
-      contentType: "application/json",
-      success: function(data) {
-        if (data['status'] == 'Success') {
-          game_id = data['game-id'];
-          $('#modal2').closeModal();
-          app = new App();
-          var ai = new BaseAi(app);
-          app.Init();
-        } else {
-          console.log(data);
-        }
-      }
-
-    });
-  });
-  $('#login').click(function() {
-    var username = $('#login_username').val();
-    var password = $('#login_password').val();
-    var data = {
-      username: username,
-      password: password
-    };
-    $.ajax({
-      method: 'POST',
-      url: auth_server_endpoint + '/account/login',
-      data: JSON.stringify(data),
-      dataType: "json",
-      contentType: "application/json",
-      success: function(data) {
-        if (data['status'] == 'Success') {
-          auth_id = data['uid'];
-          $('#modal1').closeModal();
-          $('#modal2').openModal({
-            dismissible: false
-          });
-          console.log(auth_id);
-        } else {
-          console.log(data);
-        }
-      }
-
-    });
-  });
-  $('#signup').click(function() {
-    var username = $('#signup_username').val();
-    var password = $('#signup_password').val();
-    var data = {
-      username: username,
-      password: password
-    };
-    $.ajax({
-      method: 'POST',
-      url: auth_server_endpoint + '/account/create',
-      data: JSON.stringify(data),
-      dataType: "json",
-      contentType: "application/json",
-      success: function(data) {
-        if (data['status'] == 'Success') {
-          auth_id = data['uid'];
-          $('#modal1').closeModal();
-          $('#modal2').openModal({
-            dismissible: false
-          });
-          console.log(auth_id);
-        } else {
-          console.log(data);
-        }
-      }
-
-    });
-  });
+  app = new App();
+  var ai = new BaseAi(app);
+  app.Init();
 });
