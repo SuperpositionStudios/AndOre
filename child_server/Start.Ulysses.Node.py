c6e378ac15977eba8d3127d84433fe96705ddf1d
JCharante <jcharante@gmail.com>
2016-10-22 11:53:40 -0400

    Modified child server start so it gracefully exits if Sleipnir is unreachable

--- child_server/Start.Ulysses.Node.py
+++ child_server/Start.Ulysses.Node.py
@@ -10,16 +10,19 @@ node_address = '{domain}:{port}'.format(domain=config.address, port=node_port)
 # Global Variables
 node = None
 
-# Communicate to Master Server
-req = requests.post(config.master_address + '/register/node', json={
-    'key': config.keys['node'],
-    'name': node_name,
-    'address': node_address
-})
-master_response = req.json()
-if master_response['Successful_Request']:
-    print('Successfully Communicated with Master Server')
-    print('Nodes: ', master_response['nodes'])
-    node = _node.Node(config.address, master_response['nodes'], node_name, config.keys, port=node_port)
-else:
-    print('Failed to establish connection to Master Server')
+try:
+    # Communicate to Master Server
+    req = requests.post(config.master_address + '/register/node', json={
+        'key': config.keys['node'],
+        'name': node_name,
+        'address': node_address
+    })
+    master_response = req.json()
+    if master_response['Successful_Request']:
+        print('Successfully Communicated with Sleipnir')
+        print('Nodes: ', master_response['nodes'])
+        node = _node.Node(config.address, master_response['nodes'], node_name, config.keys, port=node_port)
+    else:
+        print('Communicated with Sleipnir, but register node request failed')
+except:
+    print('Failed to establish connection to Sleipnir')
