19f3d55e328df5f4ddaa04e8ec28076ec6f78ba9
JCharante <jcharante@gmail.com>
2016-11-05 08:37:12 -0400

    Fixed installed_prerequisites.sh but adding a missing semicolon

--- deploy/install_prerequisites.sh
+++ deploy/install_prerequisites.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo apt install -y python python3 python-pip python3-pip screen vnstat nginx libffi-dev git;
+sudo apt install -y python python3 python-pip python3-pip screen vnstat nginx libffi-dev git htop;
 pip3 install --upgrade pip;
 pip2 install --upgrade pip;
 pip2 install sultan;
 git config --global user.email "you@example.com";
 git config --global user.name "Your Name";
-cd ~
+cd ~;
 git clone https://github.com/baxter-cs/AndOre.git;
 cd AndOre;
 git checkout sphere;
