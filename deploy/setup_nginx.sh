ee68d7760b4d30b5e4b21aeaeaaeeb1e32e75017
JCharante <jcharante@gmail.com>
2016-11-05 08:37:44 -0400

    Added support for websocket in setup_nginx.sh

--- deploy/setup_nginx.sh
+++ deploy/setup_nginx.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-# $1 = number of server blocks, (1 or 2, sleipnir only needs 2)
-# $2 = port for server block 1.
-# $3 = domain for server block 1
-# $4 = port for server block 2
-# $5 = domain for server block 2
+# $1 = 1 if websocket, 0 if normal http
+# $2 = number of server blocks, (1 or 2, sleipnir only needs 2)
+# $3 = port for server block 1.
+# $4 = domain for server block 1
+# $5 = port for server block 2
+# $6 = domain for server block 2
 echo -e "\nBacking up nginx config...\n"
 
 cd /etc/nginx/sites-enabled/;
@@ -13,24 +14,38 @@ sudo touch default;
 
 echo -e "Writing nginx files...\n"
 
-echo -e "Setting up nginx with $1 server blocks.\n"
+echo -e "Setting up nginx with $2 server blocks.\n"
 
-if [ $1 == 1 ]
+if [ $2 == 1 ]
 then
-	echo -e "First server block port: $2 domain: $3.\n";
+	echo -e "First server block port: $3 domain: $4.\n";
 
 	echo -e "server {" >> default;
+
 	echo -e "\tclient_max_body_size 999M;" >> default;
 	echo -e "\tlisten 80;" >> default;
-	echo -e "\tserver_name $3" >> default;
+	echo -e "\tserver_name $4" >> default;
 	echo -e "\taccess_log off;" >> default;
 	echo -e "\tlocation / {" >> default;
-	echo -e "\t\tproxy_pass http://127.0.0.1:$2;" >> default;
+
 	echo -e "\t\tproxy_set_header    Host            \$host;" >> default;
 	echo -e "\t\tproxy_set_header    X-Real-IP       \$remote_addr;" >> default;
-	echo -e "\t\tproxy_set_header    X-Forwarded-for \$remote_addr;" >> default;
+	#echo -e "\t\tproxy_set_header    X-Forwarded-for \$remote_addr;" >> default;
+	echo -e "\t\tproxy_set_header X-Forward-Proto \$scheme;" >> default;
 	echo -e "\t\tport_in_redirect off;" >> default;
-	echo -e "\t\tproxy_connect_timeout 300;" >> default;
+	if [ $1 == 1 ]
+	then
+		echo -e "\t\tproxy_read_timeout  36000s;" >> default;
+		echo -e "\t\t\n" >> default;
+		echo -e "\t\tproxy_http_version 1.1;" >> default;
+		echo -e "\t\tproxy_set_header Upgrade \$http_upgrade;" >> default;
+		echo -e "\t\tproxy_set_header Connection \"upgrade\";" >> default;
+		echo -e "\t\t\n" >> default;
+	else
+		echo -e "\t\tproxy_connect_timeout 300;" >> default;
+	fi
+	echo -e "\t\tproxy_pass http://127.0.0.1:$3;" >> default;
+
 	echo -e "\t}" >> default;
 	echo -e "\t" >> default;
 	echo -e "\terror_page   500 502 503 504  /50x.html;" >> default;
@@ -39,19 +54,34 @@ then
 	echo -e "\t}" >> default;
 	echo -e "}" >> default;
 else
-	echo -e "\nFirst server block port: $2 domain: $3.";
+	echo -e "\nFirst server block port: $3 domain: $4.";
+
 	echo -e "server {" >> default;
+
 	echo -e "\tclient_max_body_size 999M;" >> default;
 	echo -e "\tlisten 80;" >> default;
-	echo -e "\tserver_name $3" >> default;
+	echo -e "\tserver_name $4" >> default;
 	echo -e "\taccess_log off;" >> default;
 	echo -e "\tlocation / {" >> default;
-	echo -e "\t\tproxy_pass http://127.0.0.1:$2;" >> default;
+
 	echo -e "\t\tproxy_set_header    Host            \$host;" >> default;
 	echo -e "\t\tproxy_set_header    X-Real-IP       \$remote_addr;" >> default;
-	echo -e "\t\tproxy_set_header    X-Forwarded-for \$remote_addr;" >> default;
+	#echo -e "\t\tproxy_set_header    X-Forwarded-for \$remote_addr;" >> default;
+	echo -e "\t\tproxy_set_header X-Forward-Proto \$scheme;" >> default;
 	echo -e "\t\tport_in_redirect off;" >> default;
-	echo -e "\t\tproxy_connect_timeout 300;" >> default;
+	if [ $1 == 1 ]
+	then
+		echo -e "\t\tproxy_read_timeout  36000s;" >> default;
+		echo -e "\t\t\n" >> default;
+		echo -e "\t\tproxy_http_version 1.1;" >> default;
+		echo -e "\t\tproxy_set_header Upgrade \$http_upgrade;" >> default;
+		echo -e "\t\tproxy_set_header Connection \"upgrade\";" >> default;
+		echo -e "\t\t\n" >> default;
+	else
+		echo -e "\t\tproxy_connect_timeout 300;" >> default;
+	fi
+	echo -e "\t\tproxy_pass http://127.0.0.1:$3;" >> default;
+
 	echo -e "\t}" >> default;
 	echo -e "\t" >> default;
 	echo -e "\terror_page   500 502 503 504  /50x.html;" >> default;
@@ -60,21 +90,35 @@ else
 	echo -e "\t}" >> default;
 	echo -e "}" >> default;
 
-	echo -e "\nSecond server block port: $4 domain: $5.\n";
+	echo -e "\nSecond server block port: $5 domain: $6.\n";
 	echo -e "\n" >> default;
 
 	echo -e "server {" >> default;
+
 	echo -e "\tclient_max_body_size 999M;" >> default;
 	echo -e "\tlisten 80;" >> default;
-	echo -e "\tserver_name $5" >> default;
+	echo -e "\tserver_name $6" >> default;
 	echo -e "\taccess_log off;" >> default;
 	echo -e "\tlocation / {" >> default;
-	echo -e "\t\tproxy_pass http://127.0.0.1:$4;" >> default;
+
 	echo -e "\t\tproxy_set_header    Host            \$host;" >> default;
 	echo -e "\t\tproxy_set_header    X-Real-IP       \$remote_addr;" >> default;
-	echo -e "\t\tproxy_set_header    X-Forwarded-for \$remote_addr;" >> default;
+	#echo -e "\t\tproxy_set_header    X-Forwarded-for \$remote_addr;" >> default;
+	echo -e "\t\tproxy_set_header X-Forward-Proto \$scheme;" >> default;
 	echo -e "\t\tport_in_redirect off;" >> default;
-	echo -e "\t\tproxy_connect_timeout 300;" >> default;
+	if [ $1 == 1 ]
+	then
+		echo -e "\t\tproxy_read_timeout  36000s;" >> default;
+		echo -e "\t\t\n" >> default;
+		echo -e "\t\tproxy_http_version 1.1;" >> default;
+		echo -e "\t\tproxy_set_header Upgrade \$http_upgrade;" >> default;
+		echo -e "\t\tproxy_set_header Connection \"upgrade\";" >> default;
+		echo -e "\t\t\n" >> default;
+	else
+		echo -e "\t\tproxy_connect_timeout 300;" >> default;
+	fi
+	echo -e "\t\tproxy_pass http://127.0.0.1:$5;" >> default;
+
 	echo -e "\t}" >> default;
 	echo -e "\t" >> default;
 	echo -e "\terror_page   500 502 503 504  /50x.html;" >> default;
