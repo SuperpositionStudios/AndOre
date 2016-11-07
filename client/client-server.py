49641d06e12e9082790a32fccc7ce9990fd33c21
JCharante <jcharante@gmail.com>
2016-11-07 10:55:06 -0500

    Experimenting w/ deploying Preact JS Code w/ Flask

--- client/client-server.py
+++ client/client-server.py
@@ -7,6 +7,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return render_template("main/index.html")
+
+
+@app.route('/admin')
+def admin():
+    return render_template("admin/index.html")
+
+
+@app.route('/gameclient')
+def gameclient():
     return render_template("gameClient/index.html")
 
 app.run(debug=True, host='0.0.0.0', port=7002, threaded=True)
