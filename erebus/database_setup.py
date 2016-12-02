c200f7b74ec2b98c239b14d78c0ba42cb407aa4b
JCharante <jcharante@gmail.com>
2016-12-01 22:24:44 -0500

    Switched Erebus to use tabs rather than spaces

--- erebus/database_setup.py
+++ erebus/database_setup.py
@@ -8,21 +8,22 @@ Base = declarative_base()
 
 # The table for user accounts.
 class User(Base):
-    __tablename__ = 'Users'
-    pk = Column(Integer, primary_key=True)
-    aid = Column(String())
-    username = Column(String())
-    hashed_password = Column(String())
-    last_login = Column(DateTime)
-    # The privilege of the user,
-    # 0 = normal user,
-    # 1 = chat moderator,
-    # 2 = game moderator,
-    # 3 = senior game moderator
-    # 4 = qa
-    # 5 = admin
-    # 6 = senior admin
-    privilege = Column(String())
+	__tablename__ = 'Users'
+	pk = Column(Integer, primary_key=True)
+	aid = Column(String())
+	username = Column(String())
+	hashed_password = Column(String())
+	last_login = Column(DateTime)
+	# The privilege of the user,
+	# 0 = normal user,
+	# 1 = chat moderator,
+	# 2 = game moderator,
+	# 3 = senior game moderator
+	# 4 = qa
+	# 5 = admin
+	# 6 = senior admin
+	privilege = Column(String())
+
 
 # Create an engine that stores data in the local directory's
 # database.db file.
