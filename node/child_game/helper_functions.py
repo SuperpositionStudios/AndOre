caf6b112bbe0a9bca360f70f2c1ccc9d571d6e72
JCharante <jcharante@gmail.com>
2016-12-06 22:38:05 -0500

    Region Nodes now send their git revision shorthash to a player once they authenticated.

--- node/child_game/helper_functions.py
+++ node/child_game/helper_functions.py
@@ -1,4 +1,5 @@
 import config
+import subprocess
 
 
 def drint(text):
@@ -12,3 +13,17 @@ def flatten_2d_list(x):
 		for k in i:
 			response.append(k)
 	return response
+
+
+def get_git_revision_hash(return_bytes=False):
+	if return_bytes:
+		return subprocess.check_output(['git', 'rev-parse', 'HEAD'])
+	else:
+		return str(subprocess.check_output(['git', 'rev-parse', 'HEAD']), 'utf-8')
+
+
+def get_git_revision_short_hash(return_bytes=False):
+	if return_bytes:
+		return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'])
+	else:
+		return str(subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']), 'utf-8')
