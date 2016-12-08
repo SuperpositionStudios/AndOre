fea8fe89f7509cf152890e4bc9db073e5411eea3
JCharante <jcharante@gmail.com>
2016-12-08 17:25:17 -0500

    Updated production url in Start.X.Node.py

--- node/Start.Siwash.Node.py
+++ node/Start.Siwash.Node.py
@@ -1,3 +1,3 @@
 from node_starter import NodeStarter
 
-siwash = NodeStarter('Siwash', 7103, 'ws://siwash.green.iwanttorule.space', 'ws://localhost:7103', star_gates=['Toivo'])
+siwash = NodeStarter('Siwash', 7103, 'ws://siwash.green.andore.online', 'ws://localhost:7103', star_gates=['Toivo'])
--- node/Start.Voytek.Node.py
+++ node/Start.Voytek.Node.py
@@ -1,4 +1,4 @@
 from node_starter import NodeStarter
 
-voytek = NodeStarter('Voytek', 7102, 'ws://voytek.yellow.iwanttorule.space', 'ws://localhost:7102',
+voytek = NodeStarter('Voytek', 7102, 'ws://voytek.yellow.andore.online', 'ws://localhost:7102',
 					 star_gates=['Toivo'])
